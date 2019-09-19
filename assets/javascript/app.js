e9f838612c939b05869fea0e3a5cc924446ea9ea
murnda <mdelong333@gmail.com>
2019-09-19 16:21:24 -0400

    initial file creation, linking css and js, pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//Timed questions - shown one at a time
+//Variables for correct and incorrect answers to be displayed at end of game
+var correct = 0;
+var incorrect = 0;
+
+//create html element populated with a question
+//create html elements populated with answers
+//only one answer can be selected per question
+//timer counts down on each question 
+//if player selects correct answer show screen congratulating them
+//if player selects wrong answer show screen telling them the answer is wrong and display correct answer
+//if the timer runs out before the player selects an answer - tell player time has run out and display correct answer
+//after a few seconds show next question - repeat
+//on final screen show how many correct and incorrect answers player gave and give option to restart game
